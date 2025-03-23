31f468e4a54c556cdfc3ffc5bdbf64a273d39436
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2025-03-23 20:59:26 +0100

    feat: add environment config and use netlify env vars

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import { allSiteMeta } from 'contentlayer/generated';
 
+import env from '@/environment';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -17,6 +18,10 @@ const geistMono = Geist_Mono({
 
 const { basicMeta, opengraph } = allSiteMeta[0]
 export const metadata: Metadata = {
+  metadataBase: new URL(env.DEPLOY_URL),
+  alternates: {
+    canonical: new URL(env.SITE_URL),
+  },
   title: basicMeta.title,
   description: basicMeta.description,
   keywords: basicMeta.keywords,
@@ -27,6 +32,8 @@ export const metadata: Metadata = {
     description: opengraph.description,
     type: opengraph.type,
     images: opengraph.image,
+    url: new URL(env.SITE_URL),
+    siteName: opengraph.siteName,
   }
 }
 
---
+++ src/environment.ts
@@ -0,0 +1,6 @@
+const env = {
+  SITE_URL: process.env.URL || 'https://localhost:3000',
+  DEPLOY_URL: process.env.DEPLOY_PRIME_URL || 'https://localhost:3000',
+}
+
+export default env
